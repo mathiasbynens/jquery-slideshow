a7023c9bad9547c69dc89d7976ad011152518a2f
Mathias Bynens <mathias@qiwi.be>
2009-12-12 12:00:55 +0100

    Moving some multi-line comments to one line

--- jquery.slideshow.js
+++ jquery.slideshow.js
@@ -15,10 +15,9 @@
   if (settings) {
    $.extend(config, settings);
   };
-  // We loop through the selected elements, in case the slideshow was called on more than one element, e.g. `$('.foo, .bar').slideShow();`
+  // We loop through the selected elements, in case the slideshow was called on more than one element e.g. `$('.foo, .bar').slideShow();`
   this.each(function() {
-   // Inside the setInterval() block, `this` references the window object
-   // (instead of the slideshow container), so we store it inside a var
+   // Inside the setInterval() block, `this` references the window object instead of the slideshow container element, so we store it inside a var
    var $elem = $(this);
    // Hide all slides except the first one
    $elem.children(':gt(0)').hide(); // $elem.children().slice(1).hide(); would work as well, but is 1 byte longer
